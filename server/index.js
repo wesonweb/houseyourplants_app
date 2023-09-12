c82cac5d1b8350f58779923761ca3306e2eeb95e
Wes Mehaffy <wesonweb@gmail.com>
2023-09-12 11:08:37 +0100

    install cors and adds to index.js file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors({origin: '*'}))
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
