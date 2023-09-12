3c36b40399f31018f8f106be91cc19799bc97bbb
Wes Mehaffy <wesonweb@gmail.com>
2023-09-12 11:55:32 +0100

    adds middleware to parse json

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors({origin: '*'}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const PORT = process.env.PORT || 5000
 
