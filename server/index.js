1195929ea5501475f1a04a365df55ee36459538a
Wes Mehaffy <wesonweb@gmail.com>
2023-09-12 11:04:46 +0100

    installs dotenv package and places port in .env file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello, plant world!')
 })
 
-app.listen(3000, () => {
-  console.log('Server listening on port 5000')
+app.listen(process.env.PORT, () => {
+  console.log(`Server started and listening on port ${PORT}`)
 })
