d1711be70b16d8c8d7ca55301811de135da206f3
Wes Mehaffy <wesonweb@gmail.com>
2023-09-12 15:17:53 +0100

    installs mongoose and connects to database

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const plantRoutes = require('./routes/plants')
 
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI
 const app = express()
 app.use(cors({origin: '*'}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const PORT = process.env.PORT || 5000
 
 app.use('/api/plants', plantRoutes)
 
-app.listen(`${PORT}`, () => {
-  console.log(`Server started and listening on port ${PORT}`)
-})
+mongoose.connect(`${MONGO_URI}`)
+  .then(()=>{
+    app.listen(`${PORT}`, () => {
+    console.log(`Connected to db, server started and listening on port ${PORT}`)
+    })
+  })
+  .catch(err => console.log(err))
