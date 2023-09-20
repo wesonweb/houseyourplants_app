f666d40f25e2b9e277c8610ecaa6964322ccc466
Wes Mehaffy <wesonweb@gmail.com>
2023-09-20 17:48:17 +0100

    adds react-router-dom, creates RouterProvider and router, creates home page with welcome text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
+import {  // only use for react-router-dom v6 and above
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+  } from 'react-router-dom'
+
+// layout imports
+import RootLayout from './layouts/RootLayout'
+// page imports
+import Home from './pages/Home'
+
 import './App.css'
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+    )
+  )
 
+function App() {
   return (
-    <header>
-      <h1>Houseyourplants</h1>
-      <p>A web app to help you find the plant that's perfect for your home</p>
-    </header>
+    <RouterProvider router = {router} />
   )
 }
-
 export default App
