48708f05a492e71ffff3eab662b69b1318ca488c
Wes Mehaffy <wesonweb@gmail.com>
2023-09-20 16:03:43 +0100

    installs Vite and removes unrequired files and default content

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <header>
+      <h1>Houseyourplants</h1>
+      <p>A web app to help you find the plant that's perfect for your home</p>
+    </header>
+  )
+}
+
+export default App
