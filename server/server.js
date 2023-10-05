1145061487e27ee315086a0f0113b88b18a7da51
Wes Mehaffy <wesonweb@gmail.com>
2023-10-05 17:32:00 +0100

    increases header size to fix error and amends how dotenv is imported

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const plantRoutes = require('./routes/plants')
 
+
+dotenv.config()
+
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 const app = express()
 app.use(cors({origin: '*'}))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({extended: true, limit: '50mb'}))
 
 
 app.use('/api/plants', plantRoutes)
