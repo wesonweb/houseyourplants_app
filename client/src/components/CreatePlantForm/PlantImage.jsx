81ef79764fa2e9b9871e3b02f8b420a6bf38e562
Wes Mehaffy <wesonweb@gmail.com>
2023-10-05 17:39:32 +0100

    removes React Hook Form for image state management and replaces with useState; displays a preview of the image being sent from the form

--- client/src/components/CreatePlantForm/PlantImage.jsx
+++ client/src/components/CreatePlantForm/PlantImage.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-const PlantImage = ({ register }) => {
+const PlantImage = ({ plantImage, handleImageUpload }) => {
   return (
     <div className="my-6">
       <label
@@ -7,7 +7,9 @@ const PlantImage = ({ register }) => {
         className="block text-sm font-medium leading-6 text-gray-900">
         Image of the plant
       </label>
+      {!plantImage && (
         <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+
           <div className="text-center">
             <div className="mt-4 flex text-sm leading-6 text-gray-600">
               <label
@@ -17,21 +19,26 @@ const PlantImage = ({ register }) => {
               <span>Upload an image</span>
               <input
                 id="image"
-                {...register("image")}
+                onChange={handleImageUpload}
                 type="file"
+                accept="image/*"
                 className="sr-only"
                 />
               </label>
-              <p className="pl-1">or drag and drop</p>
             </div>
             <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 5MB</p>
           </div>
         </div>
+        )}
+          {plantImage && (
+            <img src={plantImage} alt="plant" className="mt-4" />
+          )}
       </div>
   )
 }
 PlantImage.propTypes = {
-  register: PropTypes.func.isRequired,
+  plantImage: PropTypes.string.isRequired,
+  handleImageUpload: PropTypes.func.isRequired
 }
 
 export default PlantImage
