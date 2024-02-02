a57a3d06526cd0479f46f2ea8d412dfb589f4444
Wes Mehaffy <wesonweb@gmail.com>
2024-02-02 15:25:14 +0000

    adds the plant's botanical name to each plant's card

--- client/src/components/Plant/PlantCard.jsx
+++ client/src/components/Plant/PlantCard.jsx
@@ -2,10 +2,11 @@ import { PropTypes } from 'prop-types'
 
 function PlantCard({ plant }) {
 
-  const { commonName, flowers, toxicity } = plant
+  const { commonName, scientificName, flowers, toxicity } = plant
   return (
     <article className="container mt-4 p-4">
       <h1 className="text-2xl mb-2">{commonName}</h1>
+      <span><em>{scientificName}</em></span>
       <p>{flowers && (
         <span className="text-green-500">This plant flowers</span>
       )}</p>
