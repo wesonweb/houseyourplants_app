5108f94a7e32d66578450f5e4f8b6bb0c135b944
Wes Mehaffy <wesonweb@gmail.com>
2024-02-13 14:16:27 +0000

    adds function to check if user exists and if not hashes password; function creates user in database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,22 @@
-const User = require('../models/UserModel')
+const User = require('../models/userModel')
 
 // login user
 const loginUser = async (req, res) => {
     res.json({message: 'login user route'})
+
 }
 
 // register user
 const registerUser = async (req, res) => {
-    res.json({message: 'register user route'})
+    const { username, email, password } = req.body
+    try {
+        const user = await User.register(username, email, password)
+        console.log(user)
+        res.status(201).json({username, email, user})
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({message: error.message})
+    }
 }
 
 module.exports = {
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const bcrypt = require('bcrypt')
+
 const userSchema = new Schema({
     username: {
         type: String,
@@ -17,5 +19,19 @@ const userSchema = new Schema({
         required: true
     }
 }, { timestamps: true })
+
+userSchema.statics.register = async function (username, email, password) {
+    const userExists = await this.findOne({ email })
+    if (userExists) {
+        throw Error ('User already exists')
+    }
+    // create the salt
+    const salt = await bcrypt.genSalt(10)
+    // hash the password
+    const hash = await bcrypt.hash(password, salt)
+    // create the user
+    const user = await this.create({ username, email, password: hash })
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
