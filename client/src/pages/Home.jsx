6548f6256e868ec2512fe74eb79f10b6f35c4742
Wes Mehaffy <wesonweb@gmail.com>
2024-02-27 09:41:13 +0000

    adds accessibility aria descriptions to modal

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,47 +4,46 @@ import { usePlantsContext } from '../hooks/usePlantsContext'
 import Loader from '../components/Loader/Loader'
 
 const Home = () => {
-  const [isLoading, seIsLoading] = useState(true)
-  const { plants, dispatch } = usePlantsContext()
-  useEffect( () => {
+    const [isLoading, seIsLoading] = useState(true)
+    const { plants, dispatch } = usePlantsContext()
+    useEffect( () => {
     const fetchPlants = async () => {
-      try {
-          const response = await fetch('http://localhost:4000/api/plants/')
-          const data = await response.json()
-          if(response.ok) {
-            seIsLoading(false)
-            dispatch({ type: 'GET_PLANTS', payload: data })
-          }
-        }
-
-      catch (error) {
+        try {
+            const response = await fetch('http://localhost:4000/api/plants/')
+            const data = await response.json()
+            if(response.ok) {
+                seIsLoading(false)
+                dispatch({ type: 'GET_PLANTS', payload: data })
+                }
+            }
+        catch (error) {
         console.error(error)
-      }
+        }
     }
 
     fetchPlants()
 
-  }, [dispatch])
+    }, [dispatch])
 
 
-  return (
+    return (
     <>
-      <article className="container mx-auto plants">
+        <article className="container mx-auto plants">
         <h1 className="text-4xl">Houseyourplants</h1>
         <p>Find the plant that is perfect for your home</p>
-          {isLoading && <Loader />}
+            {isLoading && <Loader />}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {plants && plants.map(plant => (
-              <PlantCard
-              key={plant._id}
-              plant={plant}
-              />
+            {plants && plants.map(plant => (
+                <PlantCard
+                key={plant._id}
+                plant={plant}
+                />
             ))
-          }
-          </div>
-      </article>
+            }
+            </div>
+        </article>
     </>
-  )
+    )
 }
 
 export default Home
