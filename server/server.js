80ad692a67cfe6a9e8cbd80a8160856d7c858665
Wes Mehaffy <wesonweb@gmail.com>
2024-03-04 11:37:49 +0000

    updates package files

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
 const path = require('path')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const plantRoutes = require('./routes/plants')
 const userRoutes = require('./routes/user')
 
+require('dotenv').config()
+
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 const DEVELOPMENT = process.env.NODE_ENV === 'development'
+
 const app = express()
 app.use(cors({origin: '*'}))
 app.use(express.json({limit: '50mb'}))
