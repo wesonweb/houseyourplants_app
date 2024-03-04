e0e500f16021ebd7243f8574b630361c9f13a8c7
Wes Mehaffy <wesonweb@gmail.com>
2024-03-04 10:01:28 +0000

    updates files for deployment

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
+const DEVELOPMENT = process.env.NODE_ENV === 'development'
 const app = express()
 app.use(cors({origin: '*'}))
 app.use(express.json({limit: '50mb'}))
@@ -20,10 +21,12 @@ app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use('/api/plants', plantRoutes)
 app.use('/api/user', userRoutes)
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+if (!DEVELOPMENT) {
+    app.use(express.static(path.join(__dirname, 'client/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'))
     })
+}
 
 mongoose.connect(MONGO_URI)
   .then(() => {
