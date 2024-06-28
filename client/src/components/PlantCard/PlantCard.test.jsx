29f4c2fedd75b6fceeebc4a9a0de39edf194c87d
Wes Mehaffy <wesonweb@gmail.com>
2024-06-28 16:07:13 +0100

    adds describe to PlantCard test

--- client/src/components/PlantCard/PlantCard.test.jsx
+++ client/src/components/PlantCard/PlantCard.test.jsx
@@ -4,27 +4,29 @@ import { HelmetProvider } from 'react-helmet-async'
 import { MemoryRouter } from 'react-router-dom'
 import PlantCard from './PlantCard'
 
-it('contains elements required for PlantCard', () => {
-    const mockPlant = {
-        commonName: 'Aloe Vera',
-        _id: '1',
-        scientificName: 'Aloe Vera',
-        image: {
-            url: 'https://via.placeholder.com/150'
+describe('PlantCard', () => {
+    it('contains elements required for PlantCard', () => {
+        const mockPlant = {
+            commonName: 'Aloe Vera',
+            _id: '1',
+            scientificName: 'Aloe Vera',
+            image: {
+                url: 'https://via.placeholder.com/150'
+            }
         }
-    }
-    render(
-        <HelmetProvider>
-            <MemoryRouter>
-                <PlantCard plant={mockPlant}/>
-            </MemoryRouter>
-        </HelmetProvider>
-    )
+        render(
+            <HelmetProvider>
+                <MemoryRouter>
+                    <PlantCard plant={mockPlant}/>
+                </MemoryRouter>
+            </HelmetProvider>
+        )
 
-    const plantCardHeading = screen.getByRole('heading', {name: /Aloe Vera/i})
-    expect(plantCardHeading).toBeInTheDocument()
-    const plantScientificName = screen.getByTestId('scientificName')
-    expect(plantScientificName).toBeInTheDocument()
-    const plantCardArticle = screen.getByRole('article')
-    expect(plantCardArticle).toHaveStyle({backgroundImage: 'url(https://via.placeholder.com/150)'})
+        const plantCardHeading = screen.getByRole('heading', {name: /Aloe Vera/i})
+        expect(plantCardHeading).toBeInTheDocument()
+        const plantScientificName = screen.getByTestId('scientificName')
+        expect(plantScientificName).toBeInTheDocument()
+        const plantCardArticle = screen.getByRole('article')
+        expect(plantCardArticle).toHaveStyle({backgroundImage: 'url(https://via.placeholder.com/150)'})
+    })
 })
